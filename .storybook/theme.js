792cd7f428ec94b896f13917fba7f0bb8af83d9e
Kelvin Kiptum Kiprop <kelvin.kiprop96@gmail.com>
2023-02-14 00:46:58 +0300

    - updated storybook with brand name

---
+++ .storybook/theme.js
@@ -0,0 +1,10 @@
+import { create } from '@storybook/theming';
+import Logoimg from '../public/favicon-32x32.png';
+
+export default create({
+	base: 'light',
+	brandTitle: 'Blook components',
+	brandUrl: 'https://github.com/kelvins-lab/blook-frontend',
+	// brandImage: Logoimg,
+	brandTarget: '_blank',
+});
